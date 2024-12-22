a8fd79a68ee21989d416ce6e2616048e917fe222
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2024-12-22 16:02:12 +0100

    Implement logic for Wikinimous

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's generally better to put your application-specific code in separate files in this directory
+// and require them here.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "turbo", to: "https://cdn.skypack.dev/@hotwired/turbo"
